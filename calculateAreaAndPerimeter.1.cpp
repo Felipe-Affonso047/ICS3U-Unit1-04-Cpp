f7e60b43a0ab82f083f1e922aaf90e5b231d8908
Felipe Affonso <felipe.affonso@mths.ca>
2021-04-26 18:10:20 +0000

    math

---
+++ calculateAreaAndPerimeter.1.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2021 Felipe Affonso All rights reserved
+//
+// Created by: Felipe Affonso
+// Created on: april 2021
+// This is the program that calculates area and perimeter of a rectangle
+
+#include <iostream>
+#include <cmath>
+
+int main()    {
+    std::cout << "What is the perimeter and area of a 5 cm by 3 cm rectangle?"
+    << std::endl;
+    std::cout << "Area:" << (3*5) << std::endl;
+    std::cout << "Perimeter:" << (2*(3+5)) << std::endl;
+}
---
+++ simpleMath.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Felipe Affonso All rights reserved
+//
+// Created by: Felipe Affonso
+// Created on: april 2021
+// This is the "Simple Math" program, with proper style
+
+#include <iostream>
+#include <cmath>
+
+int main()    {
+    std::cout << "3+6=" << (3+6) << std::endl;
+    std::cout << "7-2=" << (7-2) << std::endl;
+    std::cout << "8/2=" << (8/2) << std::endl;
+    std::cout << "2+6*4=" << (2+6*4) << std::endl;
+    std::cout << "pow(2,64)=" << (pow(2 , 64)) << std::endl;
+}
